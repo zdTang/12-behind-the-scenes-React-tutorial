a9c2a6536ae2dc5d0f8cbe7281cffeff85d01ea4
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-16 08:08:26 -0500

    12-003 Component Updates In Action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  console.log("App loaded!");
+  const [isDisplay, setIsDisplay] = useState(false);
+  const clickHandler = (e) => {
+    console.log("state changed.");
+    e.preventDefault();
+    setIsDisplay((isDisplay) => !isDisplay);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {isDisplay && <h1>Maybe dispaly</h1>}
+      <button onClick={clickHandler}>Click me</button>
     </div>
   );
 }

3e984b6c8e8b0ffd566bc9955ccba3d60aadcdbb
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-18 14:57:00 -0500

    12-011 Optimizing with useMemo
    here, the sort function will depends on "items" ,   which is an array, and a reference type.
    even if an Array keep the same items,   as it is reference type, an new  created array is always different.
    
    so that to make the Array looks like no-change, we can still use the useMemo( )  to reserve it .

--- src/components/Demo/DemoList.js
+++ src/components/Demo/DemoList.js
@@ -5,13 +5,10 @@ import classes from "./DemoList.module.css";
 const DemoList = (props) => {
   const { items } = props;
 
-  const sortedList = items.sort((a, b) => a - b);
-  console.log("DemoList RUNNING");
-
-  // const sortedList = useMemo(() => {
-  //   console.log('Items sorted');
-  //   return items.sort((a, b) => a - b);
-  // }, [items]);
+  const sortedList = useMemo(() => {
+    console.log("Items sorted");
+    return items.sort((a, b) => a - b);
+  }, [items]);
 
   return (
     <div className={classes.list}>
